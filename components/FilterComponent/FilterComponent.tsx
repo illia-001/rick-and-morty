a58cbc3bf39ed57c3aadc729eceea3b3b43cf563
Illia Tryndiuk <dedmoroz@Deds-MacBook-Pro.local>
2026-05-07 17:57:40 +0300

    fix issues

--- components/FilterComponent/FilterComponent.tsx
+++ components/FilterComponent/FilterComponent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Filters } from "./filters/filters";
 
@@ -7,7 +8,10 @@ const FilterComponent = () => {
       <div className="sticky top-25">
         <div className="flex flex-col items-center border-2 rounded-xl border-blue-500 overflow-hidden p-4">
           <h2 className="text-2xl font-bold text-center mb-4">Filters</h2>
-          <Link href={"/characters/1"} className="text-blue-500 hover:underline mb-4">
+          <Link
+            href={"/characters/1"}
+            className="text-blue-500 hover:underline mb-4"
+          >
             Clear filters
           </Link>
           <Filters />
